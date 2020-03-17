97129a4361f174bc67e22aff6b875f11c649c12c
Danilo Fuchs <danilo_fuchs@hotmail.com>
2020-03-17 18:45:46 -0300

    Implement 16 bit register

--- Makefile
+++ Makefile
@@ -11,4 +11,12 @@ ports:
 	ghdl -e ports
 	ghdl -a src/ports_tb.vhd
 	ghdl -e ports_tb
+	ghdl -r ports_tb --wave=src/ports_tb.ghw
 	# gtkwave src/ports_tb.ghw
+
+reg16bits:
+	ghdl -a src/reg16bits.vhd
+	ghdl -e reg16bits
+	ghdl -a src/reg16bits_tb.vhd 
+	ghdl -e reg16bits_tb 
+	ghdl -r reg16bits_tb --stop-time=3000ns --wave=src/reg16bits_tb.ghw 
---
+++ src/reg16bits.vhd
@@ -0,0 +1,30 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity reg16bits is
+    port (
+        clk : in std_logic;
+        rst : in std_logic;
+        wr_en : in std_logic;
+        data_in : in unsigned(15 downto 0);
+        data_out : out unsigned(15 downto 0)
+    );
+end entity reg16bits;
+
+architecture a_reg16bits of reg16bits is
+    signal reg : unsigned(15 downto 0);
+begin
+    process (clk, rst, wr_en)
+    begin
+        if rst = '1' then
+            reg <= "0000000000000000";
+        elsif wr_en = '1' then
+            if rising_edge(clk) then
+                reg <= data_in;
+            end if;
+        end if;
+    end process;
+
+    data_out <= reg;
+end architecture a_reg16bits;
