3da91b9ea53a93da98b25ef97ebff8931acfc15a
Danilo Fuchs <danilo_fuchs@hotmail.com>
2020-11-02 18:44:57 -0300

    Fix prime inconsistencies

--- prime.asm
+++ prime.asm
@@ -5,29 +5,27 @@
 	.data
 array:	.space	128
 	.text
-addi	$t2,$zero,32
+addi	$t2,$zero,33
 addi	$t3,$zero,1
 seed:	sb	$t3,array($t3)
 	addi 	$t3,$t3,1
 	slt	$t4,$t3,$t2
 	beq	$t4,1,seed
-
-	addi	$t1,$zero,0
 	
 	addi	$t3,$zero,4
-rm2:	sb	$t1,array($t3)
+rm2:	sb	$zero,array($t3)
 	addi 	$t3,$t3,2
 	slt	$t4,$t3,$t2
 	beq	$t4,1,rm2
 
 	addi	$t3,$zero,9
-rm3:	sb	$t1,array($t3)
+rm3:	sb	$zero,array($t3)
 	addi 	$t3,$t3,3
 	slt	$t4,$t3,$t2
 	beq	$t4,1,rm3
 
 	addi	$t3,$zero,25
-rm5:	sb	$t1,array($t3)
+rm5:	sb	$zero,array($t3)
 	addi 	$t3,$t3,5
 	slt	$t4,$t3,$t2
 	beq	$t4,1,rm5
@@ -37,7 +35,8 @@ rm5:	sb	$t1,array($t3)
 print:	addi	$t3,$t3,1
 	lb	$t1,array($t3)
 	sb	$t1,array($t5)
-	beq	$t3,$t2,end
-	j	print
+	slt	$t4,$t3,$t2
+	beq	$t4,1,print
+	j	end
 	
-end:	nop
+end:	j end
